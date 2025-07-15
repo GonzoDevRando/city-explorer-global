c2ed34a1b69da8ecfb730da94c1c03816df0fe32
GonzoDevRando <70234096+GonzoDevRando@users.noreply.github.com>
2025-07-14 21:30:59 -0400

    Update build.js
    messy fix

--- .github/build.js
+++ .github/build.js
@@ -1,16 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
-const indexPath = path.join(__dirname, 'mvp', 'index.html');
+// Go up two levels: workflows -> .github -> root
+const rootDir = path.join(__dirname, '..', '..');
+const indexPath = path.join(rootDir, 'mvp', 'index.html');
 const mapboxToken = process.env.MAPBOX_TOKEN;
 
 fs.readFile(indexPath, 'utf8', (err, data) => {
   if (err) {
     return console.error(err);
   }
-
   const result = data.replace(/__MAPBOX_TOKEN__/g, mapboxToken);
-
   fs.writeFile(indexPath, result, 'utf8', (err) => {
     if (err) return console.error(err);
     console.log('Mapbox token injected successfully');
